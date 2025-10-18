0c357cf72be2d1b6224b5ccab813003a640e9535
seongjh1105 <seongjh1105@gmail.com>
2025-10-18 22:22:41 +0900

    initial commit

---
+++ titanic_dataset.py
@@ -0,0 +1,191 @@
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+# 출력 폭 제한을 해제 (무제한)
+pd.set_option("display.width", None)
+# 모든 컬럼 출력되도록 설정
+pd.set_option("display.max_columns", None)
+
+class TitanicDataset(Dataset):
+  def __init__(self, X, y):
+    self.X = torch.FloatTensor(X)
+    self.y = torch.LongTensor(y)
+
+  def __len__(self):
+    return len(self.X)
+
+  def __getitem__(self, idx):
+    feature = self.X[idx]
+    target = self.y[idx]
+    return {'input': feature, 'target': target}
+
+  def __str__(self):
+    str = "Data Size: {0}, Input Shape: {1}, Target Shape: {2}".format(
+      len(self.X), self.X.shape, self.y.shape
+    )
+    return str
+
+
+class TitanicTestDataset(Dataset):
+  def __init__(self, X, passenger_ids):
+    self.X = torch.FloatTensor(X)
+    self.passenger_ids = passenger_ids
+
+  def __len__(self):
+    return len(self.X)
+
+  def __getitem__(self, idx):
+    feature = self.X[idx]
+    pid = self.passenger_ids[idx]
+    return {'input': feature, 'PassengerId': pid}
+
+  def __str__(self):
+    str = "Data Size: {0}, Input Shape: {1}".format(
+      len(self.X), self.X.shape
+    )
+    return str
+
+
+def get_preprocessed_dataset():
+    CURRENT_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+    train_data_path = os.path.join(CURRENT_FILE_PATH, "train.csv")
+    test_data_path = os.path.join(CURRENT_FILE_PATH, "test.csv")
+
+    train_df = pd.read_csv(train_data_path)
+    test_df = pd.read_csv(test_data_path)
+
+    test_passenger_ids = test_df["PassengerId"].reset_index(drop=True).values
+
+    all_df = pd.concat([train_df, test_df], sort=False, ignore_index=True)
+
+    all_df = get_preprocessed_dataset_1(all_df)
+
+    all_df = get_preprocessed_dataset_2(all_df)
+
+    all_df = get_preprocessed_dataset_3(all_df)
+
+    all_df = get_preprocessed_dataset_4(all_df)
+
+    all_df = get_preprocessed_dataset_5(all_df)
+
+    all_df = get_preprocessed_dataset_6(all_df)
+
+    print(all_df.columns)
+    print(all_df.head(10))
+
+    train_X = all_df[~all_df["Survived"].isnull()].drop("Survived", axis=1).reset_index(drop=True)
+    train_y = train_df["Survived"]
+    test_X = all_df[all_df["Survived"].isnull()].drop("Survived", axis=1).reset_index(drop=True)
+    
+    dataset = TitanicDataset(train_X.values, train_y.values)
+    print(dataset)
+
+    train_dataset, validation_dataset = random_split(dataset, [0.8, 0.2])
+    test_dataset = TitanicTestDataset(test_X.values, test_passenger_ids)
+    #print(test_dataset)
+
+    return train_dataset, validation_dataset, test_dataset
+
+
+def get_preprocessed_dataset_1(all_df):
+    # Pclass별 Fare (요금) 평균값을 사용하여 Fare 결측치 메우기
+    Fare_mean = all_df[["Pclass", "Fare"]].groupby("Pclass").mean().reset_index()
+    Fare_mean.columns = ["Pclass", "Fare_mean"]
+    all_df = pd.merge(all_df, Fare_mean, on="Pclass", how="left")
+    all_df.loc[(all_df["Fare"].isnull()), "Fare"] = all_df["Fare_mean"]
+    all_df = all_df.drop(columns=["Fare_mean"])
+
+    return all_df
+
+
+def get_preprocessed_dataset_2(all_df):
+    # name을 세 개의 컬럼으로 분리하여 다시 all_df에 합침
+    name_df = all_df["Name"].str.split("[,.]", n=2, expand=True)
+    name_df.columns = ["family_name", "title", "name"]
+    name_df["family_name"] = name_df["family_name"].str.strip()
+    name_df["title"] = name_df["title"].str.strip()
+    name_df["name"] = name_df["name"].str.strip()
+    all_df = pd.concat([all_df, name_df], axis=1)
+
+    return all_df
+
+
+def get_preprocessed_dataset_3(all_df):
+    # title별 Age 평균값을 사용하여 Age 결측치 메우기
+    title_age_mean = all_df[["title", "Age"]].groupby("title").median().round().reset_index()
+    title_age_mean.columns = ["title", "title_age_mean", ]
+    all_df = pd.merge(all_df, title_age_mean, on="title", how="left")
+    all_df.loc[(all_df["Age"].isnull()), "Age"] = all_df["title_age_mean"]
+    all_df = all_df.drop(["title_age_mean"], axis=1)
+
+    return all_df
+
+
+def get_preprocessed_dataset_4(all_df):
+    # 가족수(family_num) 컬럼 새롭게 추가
+    all_df["family_num"] = all_df["Parch"] + all_df["SibSp"]
+
+    # 혼자탑승(alone) 컬럼 새롭게 추가
+    all_df.loc[all_df["family_num"] == 0, "alone"] = 1
+    all_df["alone"].fillna(0, inplace=True)
+
+    # 학습에 불필요한 컬럼 제거
+    all_df = all_df.drop(["PassengerId", "Name", "family_name", "name", "Ticket", "Cabin"], axis=1)
+
+    return all_df
+
+
+def get_preprocessed_dataset_5(all_df):
+    # title 값 개수 줄이기
+    all_df.loc[
+    ~(
+            (all_df["title"] == "Mr") |
+            (all_df["title"] == "Miss") |
+            (all_df["title"] == "Mrs") |
+            (all_df["title"] == "Master")
+    ),
+    "title"
+    ] = "other"
+    all_df["Embarked"].fillna("missing", inplace=True)
+
+    return all_df
+
+
+def get_preprocessed_dataset_6(all_df):
+    # 카테고리 변수를 LabelEncoder를 사용하여 수치값으로 변경하기
+    category_features = all_df.columns[all_df.dtypes == "object"]
+    from sklearn.preprocessing import LabelEncoder
+    for category_feature in category_features:
+        le = LabelEncoder()
+        if all_df[category_feature].dtypes == "object":
+          le = le.fit(all_df[category_feature])
+          all_df[category_feature] = le.transform(all_df[category_feature])
+
+    return all_df
+
+
+if __name__ == "__main__":
+  train_dataset, validation_dataset, test_dataset = get_preprocessed_dataset()
+
+  print("train_dataset: {0}, validation_dataset.shape: {1}, test_dataset: {2}".format(
+    len(train_dataset), len(validation_dataset), len(test_dataset)
+  ))
+  print("#" * 50, 1)
+
+  train_data_loader = DataLoader(dataset=train_dataset, batch_size=16, shuffle=True)
+  validation_data_loader = DataLoader(dataset=validation_dataset, batch_size=len(validation_dataset), shuffle=True)
+  test_data_loader = DataLoader(dataset=test_dataset, batch_size=len(test_dataset))
+
+  print("[TRAIN]")
+  for idx, batch in enumerate(train_data_loader):
+    print("{0} - {1}: {2}".format(idx, batch['input'].shape, batch['target'].shape))
+
+  print("[VALIDATION]")
+  for idx, batch in enumerate(validation_data_loader):
+    print("{0} - {1}: {2}".format(idx, batch['input'].shape, batch['target'].shape))
+
+  print("[TEST]")
+  for idx, batch in enumerate(test_data_loader):
+    print("{0} - {1}".format(idx, batch['input'].shape))
